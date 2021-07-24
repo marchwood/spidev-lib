6b5cebaad8a925cf76654597ff19e0c90ee38cbc
Roger James <roger@beardandsandals.co.uk>
2021-07-24 10:09:25 +0100

    Added a .gitignore and fixed warnings

--- sample/spidev-testc.c
+++ sample/spidev-testc.c
@@ -8,7 +8,7 @@ uint8_t tx_buffer[32];
 uint8_t rx_buffer[32];
 
 
-void main( void)
+int main( void)
 {
   int spifd;
   spi_config.mode=0;
@@ -20,9 +20,9 @@ void main( void)
 
   memset(tx_buffer,0,32);
   memset(rx_buffer,0,32);
-  sprintf(tx_buffer,"hello world");
+  sprintf((char *)tx_buffer,"hello world");
   printf("sending %s, to spidev2.0 in full duplex \n ",tx_buffer); 
-  spi_xfer(spifd,tx_buffer,strlen(tx_buffer),rx_buffer,strlen(tx_buffer));
+  spi_xfer(spifd,tx_buffer,strlen((char *)tx_buffer),rx_buffer,strlen((char *)rx_buffer));
   printf("rx_buffer=%s\n",rx_buffer);
   spi_close(spifd); 
   }
